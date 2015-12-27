cebf996f223a2e45e078597cfdf0072c97dda2d0
spiderbasic <support@spiderbasic.com>
2015-12-27 11:08:14 +0200

    1.10 javascript CDN
    1.10 javascript CDN for SpiderBasic

---
+++ 1.10/javascript/main.js
@@ -0,0 +1,43 @@
+// contents of main.js
+require.config({
+    waitSeconds: 0
+});
+
+spider.nbModules++;
+
+var put;
+var SpiderLaunch;
+
+var SpiderLaunchExecuted = false;
+function SpiderMain() {
+  if (spider.nbLoadedModules == spider.nbModules && !SpiderLaunchExecuted)
+  { 
+    if (SpiderLaunch)
+    {
+      SpiderLaunchExecuted = true;
+      SpiderLaunch();
+    }
+  }
+};
+
+requirejs(['put-selector/put', 'jquery.min'],
+function (PutSelectorPut) {
+  
+  put = PutSelectorPut;
+
+  // We need to load these after jquery, as it depends on it  
+  require(['jquery-ui.custom.min', 'jquery.injectCSS'], function   () {
+             spider.nbLoadedModules++;
+             SpiderMain(); // Try to launch the main procedure (will happen if all other libs modules are loaded)
+    }
+  );
+    //jQuery, canvas and the app/sub module are all
+    //loaded and can be used here now.
+    
+  (function($) {
+   $.fn.cssValue = function(p) {
+      var result;
+      return isNaN(result = parseFloat(this.css(p))) ? 0 : result;
+   };
+  })(jQuery);
+});
